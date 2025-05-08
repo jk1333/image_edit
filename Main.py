5d17fbd566ee862a2759dafb8b229186f6f13ab1
Jeehyeok Kim <jk13@nate.com>
2025-05-08 16:55:25 +0900

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,22 +1,35 @@
 import streamlit as st
 import io
-import numpy as np
 import PIL, PIL.Image
 from PIL import ImageDraw
 from streamlit_img_label import st_img_label
 from streamlit_img_label.manage import ImageManager
-import google.cloud.aiplatform as aip
-from vertexai.preview.vision_models import Image, ImageGenerationModel
+from google.cloud import aiplatform
+from google import genai
+from google.genai import types
 
 st.set_page_config(layout='wide')
 
-LANGUAGE = ['auto', 'ko', 'en']
+LANGUAGE = ['en']
 RATIO = ["1:1", "9:16", "16:9", "4:3", "3:4"]
-UPSCALE = [2048, 4096]
+UPSCALE = ["x2", "x4"]
 MAX_IMAGE_EDIT_SIZE = 700
-MODELS = ["imagen-3.0-generate-001", "imagen-3.0-fast-generate-001"]
+GENERATION_MODELS = ["imagen-3.0-generate-002", "imagen-3.0-fast-generate-001"]
+EDIT_MODEL = "imagen-3.0-capability-001"
+UPSCALE_MODEL = "imagen-3.0-generate-002"
+GUIDANCE_SCALE = [10.0, 5.0, 20.0]
+BASE_STEPS = 75
+DILATION = 0.03
 
-aip.init()
+@st.cache_resource
+def get_client():
+    return genai.Client(vertexai=True,
+        project=aiplatform.initializer.global_config.project,
+        location=aiplatform.initializer.global_config.location)
+
+client = genai.Client(vertexai=True,
+        project=aiplatform.initializer.global_config.project,
+        location=aiplatform.initializer.global_config.location)
 
 if 'generated_image' not in st.session_state:
     st.session_state['generated_image'] = []
@@ -81,52 +94,81 @@ def rects_to_bbox(rects):
         return_rects.append([rect["left"], rect["top"], rect["left"] + rect["width"], rect["top"] + rect["height"]])
     return return_rects
 
-def generate_image(model, language, prompt, negative_prompt, ratio, seed = 0):
-    model = ImageGenerationModel.from_pretrained(model)
-    response = model.generate_images(
-        prompt=prompt,
-        negative_prompt=negative_prompt,
-        # Optional parameters
-        number_of_images=4,
-        language=language,
-        # You can't use a seed value and watermark at the same time.
-        add_watermark=True,
-        #seed=seed,
-        aspect_ratio=ratio,
-        safety_filter_level="block_few",
-        person_generation="allow_adult",
+def generate_image(model, language, prompt, negative_prompt, ratio, seed = 0, enhance_prompt = True):
+    response = get_client().models.generate_images(
+        model = model,
+        prompt = prompt,
+        config = types.GenerateImagesConfig(
+            negative_prompt = negative_prompt,
+            number_of_images = 4,
+            aspect_ratio = ratio,
+            include_rai_reason = True,
+            language = language,
+            add_watermark = True,
+            #seed = seed,
+            enhance_prompt = enhance_prompt,
+            safety_filter_level = types.SafetyFilterLevel.BLOCK_ONLY_HIGH,
+            person_generation = types.PersonGeneration.ALLOW_ADULT,
+            output_mime_type = "image/png"
+        )
     )
-    return response.images
+    return response.generated_images
 
-def upscale_image(image, new_size = 2048):
-    model = ImageGenerationModel.from_pretrained("imagen-3.0-generate-001")
-    return model.upscale_image(image, new_size)
+def upscale_image(model, image, upscale_factor = 'x2'):
+    response = get_client().models.upscale_image(
+        model = model,
+        image = image,
+        upscale_factor = upscale_factor,
+        config = types.UpscaleImageConfig(
+            include_rai_reason = True,
+            output_mime_type = "image/png"
+        )
+    )
+    return response.generated_images[0]
 
-def edit_image_mask(
-    input_image: Image,
-    mask_image: Image,
+def edit_image_mask(model,
+    input_image: types.Image,
+    mask_image: types.Image,
     language = None,
     prompt = None,
     negative_prompt = None,
     edit_mode = None,
     mask_mode = None,
-    guidance_scale = None
+    guidance_scale = None,
+    base_steps = None,
+    dilation = None
 ):
-    model = ImageGenerationModel.from_pretrained("imagegeneration@006")
-    response = model.edit_image(
-        base_image=input_image,
-        mask=mask_image,
-        prompt=prompt,
-        negative_prompt=negative_prompt,
-        edit_mode=edit_mode,
-        number_of_images=4,
-        safety_filter_level="block_few",
-        guidance_scale=guidance_scale,
-        mask_mode=mask_mode,
-        person_generation="allow_adult",
-        language = language
+    response = client.models.edit_image(
+        model = model,
+        prompt = prompt,
+        reference_images = [
+            types.RawReferenceImage(
+                reference_id = 1,
+                reference_image = input_image,
+            ),
+            types.MaskReferenceImage(
+                reference_id = 2,
+                reference_image = mask_image,
+                config = types.MaskReferenceConfig(
+                    mask_mode = mask_mode,
+                    mask_dilation = dilation
+                )
+            )
+        ],
+        config = types.EditImageConfig(
+            negative_prompt = negative_prompt,
+            number_of_images = 4,
+            guidance_scale = guidance_scale,
+            safety_filter_level = types.SafetyFilterLevel.BLOCK_ONLY_HIGH,
+            person_generation = types.PersonGeneration.ALLOW_ADULT,
+            include_rai_reason = True,
+            output_mime_type = "image/png",
+            language = language,
+            edit_mode = edit_mode,
+            base_steps = base_steps
+        )
     )
-    return response.images
+    return response.generated_images
 
 col_left, col_right = st.columns(2)
 
@@ -164,20 +206,20 @@ with col_right:
         cols = st.columns(4)
         language = cols[0].selectbox("Language", LANGUAGE)
         ratio = cols[1].selectbox("Generation Ratio", RATIO)
-        model = cols[2].selectbox("Model", MODELS)
-        if cols[3].button("Generate image\n\n(Imagen 3)", use_container_width=True):
+        model = cols[2].selectbox("Model", GENERATION_MODELS)
+        if cols[3].button("Generate image", use_container_width=True):
             with st.spinner("Generating..."):
                 st.session_state['generated_image'] = generate_image(model, language, generation_text, negative_text, ratio)
     #for edit
     with st.container(border=1):
         cols = st.columns([2, 2, 1])
-        edit_mode = cols[0].selectbox('Edit mode', ("inpainting-insert", "inpainting-remove", "outpainting", "product-image"))
-        mask_mode = cols[1].selectbox("Mask mode", ("foreground", "background", "semantic"))
-        guidance_scale = cols[2].text_input("guidance_scale", 50.0)
-        if edit_mode == "inpainting-remove":
-            mask_mode = None
-        if len(guidance_scale) == 0:
-            guidance_scale = None
+        edit_mode = cols[0].selectbox('Edit mode', ("EDIT_MODE_INPAINT_INSERTION", "EDIT_MODE_INPAINT_REMOVAL", "EDIT_MODE_OUTPAINT", "EDIT_MODE_PRODUCT_IMAGE"))
+        mask_mode = cols[1].selectbox("Mask mode", ("MASK_MODE_USER_PROVIDED", "MASK_MODE_FOREGROUND", "MASK_MODE_BACKGROUND", "MASK_MODE_SEMANTIC"))
+        guidance_scale = cols[2].selectbox("guidance_scale", GUIDANCE_SCALE)
+        #if edit_mode == "inpainting-remove":
+        #    mask_mode = None
+        #if len(guidance_scale) == 0:
+        #    guidance_scale = None
         cols = st.columns(2)
         prompt = cols[0].text_input("Edit Prompt", "")
         if len(prompt) == 0:
@@ -187,17 +229,17 @@ with col_right:
             negative_prompt = None
     
         cols = st.columns(3)
+        base_steps = int(cols[0].text_input("Base steps", BASE_STEPS))
+        dilation = float(cols[1].text_input("Dilation", DILATION))
         if st.session_state['selected_image'] != None:
             base_image = st.session_state['selected_image'].get_img()
-            new_width = int(cols[0].text_input("Outpaint width", base_image.width))
-            new_height = int(cols[1].text_input("Outpaint height", base_image.height))
-            if edit_mode != "outpainting":
-                new_width = base_image.width
-                new_height = base_image.height
-        if cols[2].button("Edit image\n\n(Imagen 2)", use_container_width=True):
+            if edit_mode == "EDIT_MODE_OUTPAINT":
+                outpaint_cols = st.columns(2)
+                new_width = int(outpaint_cols[0].text_input("Outpaint width", base_image.width))
+                new_height = int(outpaint_cols[1].text_input("Outpaint height", base_image.height))
+        if cols[2].button("Edit image", use_container_width=True):
             with st.spinner("Editing..."):
                 #By default is mask free mode
-                mask = None
                 new_base_image = base_image
 
                 #If it is masked rect based edit mode
@@ -207,10 +249,9 @@ with col_right:
                     col1, col2 = st.columns(2)
                     col1.image(base_image)
                     col2.image(mask_image)
-                    mask = Image(get_image_bytes(mask_image))
                 
-                #If target resolution is changed
-                elif (base_image.width != new_width) or (base_image.height != new_height):
+                #If EDIT_MODE_OUTPAINT
+                elif edit_mode == "EDIT_MODE_OUTPAINT":
                     mask_image = PIL.Image.new("RGBA", (new_width, new_height), color="white")
                     #black to main image, white to new
                     fill_color(mask_image, [{"left": int(new_width/2 - base_image.width/2), "top": int(new_height/2 - base_image.height/2), 
@@ -220,18 +261,18 @@ with col_right:
                     col1, col2 = st.columns(2)
                     col1.image(new_base_image)
                     col2.image(mask_image)
-                    mask = Image(get_image_bytes(mask_image))
                 
-                st.session_state['generated_image'] = edit_image_mask(Image(get_image_bytes(new_base_image)), 
-                                                                     mask, language, prompt, negative_prompt, edit_mode, mask_mode, guidance_scale)
+                st.session_state['generated_image'] = edit_image_mask(EDIT_MODEL, types.Image(image_bytes = get_image_bytes(new_base_image)), 
+                                                                     types.Image(image_bytes = get_image_bytes(mask_image)), 
+                                                                     language, prompt, negative_prompt, edit_mode, mask_mode, guidance_scale, base_steps, dilation)
 
     for idx, image in enumerate(st.session_state['generated_image']):
         with st.container(border=1):
-            pil_image = image._pil_image
+            pil_image = image.image._pil_image
             st.image(pil_image)
             st.caption(f"Width: {pil_image.width}, Height: {pil_image.height}")
             cols = st.columns(4)
-            cols[0].download_button("Download", data=image._image_bytes, file_name="image.png", key=f"download_image_{idx}", use_container_width=True)
+            cols[0].download_button("Download", data=image.image.image_bytes, file_name="image.png", key=f"download_image_{idx}", use_container_width=True)
             if cols[1].button("Edit", key=f"edit_image_{idx}", use_container_width=True):
                 st.session_state['selected_image'] = MemoryImageManager(pil_image, [])
                 st.session_state['resized_image'] = st.session_state['selected_image'].resizing_img(MAX_IMAGE_EDIT_SIZE, MAX_IMAGE_EDIT_SIZE)
@@ -240,5 +281,5 @@ with col_right:
             if pil_image.width == pil_image.height == 1024:
                 upscale = cols[2].selectbox("Upscale", UPSCALE, key=f"upscale_ratio_{idx}", label_visibility="collapsed")
                 if cols[3].button("Upscale", key=f"upscale_image_{idx}", use_container_width=True):
-                    st.session_state['generated_image'][idx] = upscale_image(image, upscale)
+                    st.session_state['generated_image'][idx] = upscale_image(UPSCALE_MODEL, image.image, upscale)
                     st.rerun()
